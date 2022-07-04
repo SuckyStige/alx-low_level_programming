9af245b13092638ef65cd965f2bc7380a268a643
Arinze Success <stigehub@gmail.com>
2022-07-04 12:54:13 -0700

    Combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,16 +7,16 @@
 *
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-int d;
-for (d = 0; d < 90; d++)
+int num1, num2;
+for (num1 = 0; num1 < 10; num1++)
 {
-putchar((d / 10) + '0');
-putchar((d % 11) + '0');
-if (d != 89)
+for (num2 = 0; num2 < 10; num2++)
 {
+putchar((num1 % 10) + '0');
+putchar((num2 % 10) + '0');
+if (num1 == 9 && num2 == 9)	
 putchar(',');
 putchar(' ');
 }
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,14 +12,11 @@ int main(void)
 
 {
 int d;
-for (d = 0; d < 90; d++)
-{
-for ()
+for (d = 0; d < 100; d++)
 {
 putchar((d / 10) + '0');
 putchar((d % 10) + '0');
-}
-if (d != 89)
+if (d != 99)
 {
 putchar(',');
 putchar(' ');
