83a3ee24c82836a7dcf329fbd7005e3929aeb321
Arinze Success <stigehub@gmail.com>
2022-07-04 11:10:10 -0700

    Combination of two digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,8 +14,8 @@ int main(void)
 int d;
 for (d = 1; d < 90; d++)
 {
-putchar((d / 10) + '0');
-putchar((d % 10) + '0');
+putchar((d / 9) + '0');
+putchar((d % 9) + '0');
 if (d != 89)
 {
 putchar(',');
