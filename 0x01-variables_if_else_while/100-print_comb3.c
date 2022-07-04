b9763d4b7c3d8e0208139c775c1edae553c674fe
Arinze Success <stigehub@gmail.com>
2022-07-04 13:02:24 -0700

    Combination of numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,20 +3,20 @@
 #include <time.h>
 
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-int num1, num2;
-for (num1 = 0; num1 < 10; num1++)
+int d;
+for (d = 0; d < 100; d++)
 {
-for (num2 = 0; num2 < 10; num2++)
+putchar((d / 10) + '0');
+putchar((d % 10) + '0');
+if (d != 99)
 {
-putchar((num1 % 10) + '0');
-putchar((num2 % 10) + '0');
-if (num1 == 9 && num2 == 9)	
 putchar(',');
 putchar(' ');
 }
