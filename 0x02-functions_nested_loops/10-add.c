a34abbfc25e0d9e629bde7eb112771f7c74236af
Arinze Success <stigehub@gmail.com>
2022-07-06 13:30:25 -0700

    Function that add integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two integers and returns the result
+ * @num: first number input 
+ * @num2: second number input
+ *
+ * Return: num + num1
+ */
+int add(int num, int num1)
+{
+	return (num + num1);
+}
