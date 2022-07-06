40f33b64a5dbd6c7ff653017e4b27d79c6aa2567
Arinze Success <stigehub@gmail.com>
2022-07-06 10:05:13 -0700

    Printing x10 the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * Print_alphabet_x10 - a function that print 10 times that alphabet
+ * print_alphabet_x10 - a function that print 10 times that alphabet
  *
  * Return: x10 a -z
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int n, co;
 
