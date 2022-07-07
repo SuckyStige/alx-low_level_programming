6e660c585409f18001e9b593efcf23f8dcda44b4
Arinze Success <stigehub@gmail.com>
2022-07-07 08:59:18 -0700

    Print number from 0 up to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print the numbers from 0 up to 9
+ *
+ * Return: The numbers since 0 up to 9
+ */
+void print_numbers(void)
+{
+	int f;
+	
+	for (f = 0; f <= 9; f++)
+	{
+		_putchar(f + '0');
+	}
+
+	_putchar('\n');
+}
