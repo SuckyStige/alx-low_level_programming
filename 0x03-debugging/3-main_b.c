50552421e70e3cf3d0e2b314f12852b4ef3c2b11
Arinze Success <stigehub@gmail.com>
2022-07-08 08:06:46 -0700

    Converting the moonths and days of the year

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -2,11 +2,10 @@
 #include "main.h"
 
 /**
- * main - takes a date and prints how manys are left in the year, taking
- * leap years into account
+ * main - takes a date and prints how many days are
+ * left in the year, taking leap years into account
  * Return: 0
  */
-
 int main(void)
 {
 	int month;
