c8d7c928e9722284a78dbd8a1696e15afd6468ce
Sucky Stige <stigehub@gmail.com>
2022-08-19 17:55:16 -0700

    Converting Binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,21 +8,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int decimal = 0;
 	int str_len = 0, base = 1;
-	unsigned int dec_val = 0;
 
 	if (!check_valid_string(b))
 		return (0);
 
 	while (b[str_len] != '\0')
 		str_len++;
+
 	while (str_len)
 	{
-		dec_val += ((b[str_len - 1] - '0' * base));
+		decimal += ((b[str_len - 1] - '0') * base);
 				base *= 2;
 				str_len--;
 	}
-	return (dec_val);
+	return (decimal);
 }
 
 /**
@@ -35,6 +36,7 @@ int check_valid_string(const char *b)
 {
 if (b == NULL)
 return (0);
+
 while (*b)
 {
 if (*b != '1' && *b != '0')
