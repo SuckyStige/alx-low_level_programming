da5a0f690dd5e59cff784cbf25d87658d8565acc
Sucky Stige <stigehub@gmail.com>
2022-08-22 14:33:11 -0700

    Copying content to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,8 +16,11 @@ int main(int argc, char *argv[])
 	char buf[1024];
 
 	if (argc != 3)
+	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file_from = open(argv[1], O_RDONLY);
+	}
+
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
