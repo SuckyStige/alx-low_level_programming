dc964df3b27485bd1a8a6bda76d195db61a5f53f
Sucky Stige <stigehub@gmail.com>
2022-12-07 11:20:23 -0800

    Interview Challenge

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,40 +2,20 @@
 """ Function: island_perimeter """
 
 def island_perimeter(grid):
-    """ Returns the perimeter of the island """
-    ba = 0
-
-    if grid is None or not isinstance(grid, list):
-        return 0
-    for row in range(len(grid)):
-        for element in range(len(grid[row])):
-            if grid[row][element] == 1:
-                ba += 4
-                try:
-                    if element - 1 < 0:
-                        pass
-                    elif grid[row][element - 1] == 1:
-                        ba -= 1
-                except Exception:
-                    pass
-                try:
-                    if grid[row][element + 1] == 1:
-                        ba -= 1
-                except Exception:
-                    pass
-                try:
-                    if row - 1 < 0:
-                        pass
-                    elif grid[row - 1][element] == 1:
-                        ba -= 1
-                except Exception:
-                    pass
-                try:
-                    if row + 1 >= len(grid):
-                        pass
-                    elif grid[row + 1][element] == 1:
-                        ba -= 1
-                except Exception:
-                    pass
-    return ba
-
+    """
+    [returns the perimeter of the island described in grid]
+    """
+    terrain = 1
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+    for v_value in range(height):
+        for h_value in range(width):
+            if grid[v_value][h_value] == terrain:
+                size += 1
+                if (h_value > 0 and grid[v_value][h_value - 1] == terrain):
+                    edges += 1
+                if (v_value > 0 and grid[v_value - 1][h_value] == terrain):
+                    edges += 1
+    return size * 4 - edges * 2
